a2a70f39fe5a7a782a2484bf9ae0cd5bb3e975d3
PierreCarrione <pierre.rossi@hotmail.com>
2025-04-03 21:23:02 -0300

    Adicionado tela de Cadastro.

---
+++ src/pages/register/styles.js
@@ -0,0 +1,91 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+    width: 100%;
+    max-width: 80%;
+    margin: 0 auto;
+    margin-top: 120px;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const Wrapper = styled.div`
+    max-width: 374px;
+    height: 450px;
+    margin-top: 80px;
+    margin-left: 300px;
+`
+
+export const Column = styled.div`
+    flex: 1;
+`
+
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    //justify-content: space-between;
+    align-items: center;
+    margin-top: 20px;
+`
+
+
+
+export const Title = styled.h2`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 32px;
+    max-width: 90%;
+    margin-bottom: 20px;
+    line-height: 100%;
+    margin-left: 65px;
+
+    color: #FFFFFF;
+`
+
+export const TitleLogin = styled.p`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 32px;
+    line-height: 44px;
+    margin-bottom: 8px;
+`
+
+export const SubtitleLogin = styled.p`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 25px;
+    margin-bottom: 35px;
+`
+
+
+export const InfoText  = styled.p`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 19px;
+
+    color: #FFFFFF;
+` 
+
+export const FazerLogin = styled.span`
+  cursor: pointer;  /* Indica que é clicável */
+  font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 19px;
+
+    color: #23DD7A;
+  
+  &:hover {
+    color:rgb(15, 102, 56); /* Cor ao passar o mouse */
+  }
+`
